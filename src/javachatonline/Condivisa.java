6ac56248014529988cb9908ec76bb20b223ea62c
feeRuzz <81413649+feeRuzz@users.noreply.github.com>
2021-12-08 23:01:17 +0100

    updates

--- src/javachatonline/Condivisa.java
+++ src/javachatonline/Condivisa.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package javachatonline;
 
 import java.io.IOException;
@@ -10,10 +5,7 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.SocketException;
 
-/**
- *
- * @author kazakevych_vladyslav
- */
+
 public class Condivisa {
 
     private DatagramSocket server;
